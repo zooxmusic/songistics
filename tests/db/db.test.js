4257674885f3c536225dbea4dbcc84526fd93bda
Brian Szucs <brian@zooxmusic.com>
2025-02-03 22:17:56 -0500

    added tests

---
+++ tests/db/db.test.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { DynamoDBClient, PutItemCommand, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const TABLE_NAME = "songistics";
+
+// Test inserting a song topic
+test("Add a song topic to DynamoDB", async () => {
+  const params = {
+    TableName: TABLE_NAME,
+    Item: marshall({
+      id: "test-topic-1",
+      type: "song_topic",
+      name: "Test Topic",
+      description: "A test topic for Jest testing.",
+    }),
+  };
+
+  await client.send(new PutItemCommand(params));
+
+  // Fetch and verify sthe record
+  const getParams = {
+    TableName: TABLE_NAME,
+    Key: marshall({ id: "test-topic-1" }),
+  };
+
+  const { Item } = await client.send(new GetItemCommand(getParams));
+  expect(unmarshall(Item)).toEqual({
+    id: "test-topic-1",
+    type: "song_topic",
+    name: "Test Topic",
+    description: "A test topic for Jest testing.",
+  });
+});
